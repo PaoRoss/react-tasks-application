6610705f77c9e06fbce36f91e4a956327d1bd98d
PaoRoss <paodiazglez.pdgr@gmail.com>
2022-12-11 20:56:36 -0600

    adds new folders for improve organization

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function TaskForm({ createTask }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, description)
+    createTask({
+      title,
+      description
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Escribe tu tarea"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Esrcibe la descripción de la tarea"
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+      <button>Guardar</button>
+    </form>
+  );
+}
+export default TaskForm;
